97ae0b983b9b5701cbc88f0ffc8be0a5267c0e05
Daniel Deverell <daniel.deverell@gmail.com>
2021-06-18 14:19:48 -0400

    done

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,6 +1,16 @@
-const images = ["apples", "apples-red", "apples-group"];
-console.log(images);
-for (let image of images) {
-  const appleString = `<img src="/img/${image}.png" alt="A nice picture of apples." />`;
-  document.querySelector("pommes").append(appleString);
+function displayApples() {
+  if (!document.querySelector(".pommes")) return;
+  const images = ["apples", "apples-red", "apples-group"];
+  const appleDiv = document.querySelector(".pommes");
+  let appleString = "";
+
+  for (let image of images) {
+    appleString += `<img src="/img/${image}.png" alt="A nice picture of apples." />`;
+  }
+
+  console.log(appleString);
+
+  appleDiv.innerHTML = appleString;
 }
+
+displayApples();
